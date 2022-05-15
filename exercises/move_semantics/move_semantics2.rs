6a08293f5af1795c61743765cb5d543bd450c018
Thomas Sobieck <thomas@sobieck.com>
2022-05-15 10:29:43 -0500

    Another answer to move sem 2

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -5,9 +5,10 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = Vec::<i32>::new();
 
-    let mut vec1 = fill_vec(vec0.clone());
+    // let mut vec1 = fill_vec(vec0.clone());
+    let mut vec1 = fill_vec_ref(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -19,6 +20,16 @@ fn main() {
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+
+fn fill_vec_ref(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
