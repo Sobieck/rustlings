fce3fa8ffa62fde3ae6597ead10262a24f1b9f88
Thomas Sobieck <thomas@sobieck.com>
2022-05-15 10:35:25 -0500

    Another answer to move sem 2

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,13 +2,19 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::<i32>::new();
-
+    //answer 1
+    // let vec0 = Vec::new();
     // let mut vec1 = fill_vec(vec0.clone());
-    let mut vec1 = fill_vec_ref(&vec0);
+
+    //2
+    // let vec0 = Vec::<i32>::new();
+    // let mut vec1 = fill_vec_ref(&vec0);
+
+    //3 // output is diffent because vec0 is being filled in the function, wheras the other it is being cloned before the filling
+    let mut vec0: Vec<i32> = Vec::new();
+    let mut vec1 = fill_vec_mut_borrow(&mut vec0);
+
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -36,4 +42,23 @@ fn fill_vec_ref(vec: &Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+fn fill_vec_internal_clone(vec: Vec<i32>) -> Vec<i32> {
+    // didn't work due to borrowing stuff. I think I'm getting this now.
+    let mut vec = vec.clone();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+
+fn fill_vec_mut_borrow(vec: &mut Vec<i32>) -> Vec<i32> {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec.clone()
 }
